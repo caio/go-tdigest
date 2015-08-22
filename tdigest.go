695239f2a32532df7207aadf3ccdf6aabd2b7c87
Caio Romão <caioromao@gmail.com>
2015-08-22 16:42:41 -0300

    First commit

---
+++ tdigest.go
@@ -0,0 +1,252 @@
+package tdigest
+
+import (
+	"bytes"
+	"fmt"
+	"math"
+	"math/rand"
+
+	"github.com/ancientlore/go-avltree"
+)
+
+type Centroid struct {
+	mean  float64
+	count float64
+}
+
+func (c Centroid) String() string {
+	return fmt.Sprintf("C<m=%.6f,c=%.1f>", c.mean, c.count)
+}
+
+func (c Centroid) Equals(other Centroid) bool {
+	return c.mean == other.mean && c.count == other.count
+}
+
+func (c *Centroid) Update(x float64, weight float64) {
+	c.count += weight
+	c.mean += weight * (x - c.mean) / c.count
+}
+
+var InvalidCentroid Centroid = Centroid{mean: 0.0, count: 0}
+
+func compareCentroids(p, q interface{}) int {
+	a := p.(Centroid).mean
+	b := q.(Centroid).mean
+
+	if a < b {
+		return -1
+	} else if a > b {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func centroidLess(p, q interface{}) bool {
+	res := p.(Centroid).mean < q.(Centroid).mean
+	return res
+}
+
+func centroidLessOrEquals(p, q interface{}) bool {
+	a := p.(Centroid).mean
+	b := q.(Centroid).mean
+	return a <= b
+}
+
+type TDigest struct {
+	summary     *avltree.Tree
+	compression uint
+	count       float64
+}
+
+func New(compression uint) *TDigest {
+	tree := avltree.New(compareCentroids, 0)
+	tdigest := TDigest{compression: compression, summary: tree, count: 0}
+	return &tdigest
+}
+
+func (t *TDigest) Percentile(p float64) float64 {
+	if p < 0 || p > 1 {
+		panic("Percentiles must be between 0 and 1 (inclusive)")
+	}
+
+	if t.summary.Len() == 0 {
+		return math.NaN()
+	} else if t.summary.Len() == 1 {
+		return t.summary.At(0).(Centroid).mean
+	}
+
+	p *= t.count
+	var total float64 = 0
+	i := 0
+
+	for item := range t.summary.Iter() {
+		k := item.(Centroid).count
+
+		if p < total+k {
+			if i == 0 || i+1 == t.summary.Len() {
+				return item.(Centroid).mean
+			}
+			succ, pred := t.successorAndPredecessorItems(item.(Centroid))
+			delta := (succ.mean - pred.mean) / 2
+			return item.(Centroid).mean + ((p-total)/k-0.5)*delta
+		}
+
+		i++
+		total += k
+	}
+
+	return t.summary.At(t.summary.Len() - 1).(Centroid).mean
+}
+
+func (t *TDigest) Update(value float64, weight float64) {
+	t.count += weight
+
+	newCentroid := Centroid{value, weight}
+
+	if t.summary.Len() == 0 {
+		t.addCentroid(newCentroid)
+		return
+	}
+
+	candidates := t.findNearestCentroids(newCentroid)
+
+	for len(candidates) > 0 && weight > 0 {
+		j := rand.Intn(len(candidates))
+		chosen := candidates[j]
+
+		quantile := t.computeCentroidQuantile(chosen)
+
+		if float64(chosen.count+weight) > t.threshold(quantile) {
+			candidates = append(candidates[:j], candidates[j+1:]...)
+			continue
+		}
+
+		delta_w := math.Min(t.threshold(quantile)-float64(chosen.count), float64(weight))
+		t.updateCentroid(chosen, value, delta_w)
+		weight -= delta_w
+
+		candidates = append(candidates[:j], candidates[j+1:]...)
+	}
+
+	if weight > 0 {
+		t.addCentroid(Centroid{value, weight})
+	}
+
+	if float64(t.summary.Len()) > float64(20*t.compression) {
+		// FIXME t.compress()
+	}
+}
+
+func (t *TDigest) Compress() {
+	if t.summary.Len() <= 1 {
+		return
+	}
+}
+
+func (t TDigest) String() string {
+	var buffer bytes.Buffer
+	for c := range t.summary.Iter() {
+		buffer.WriteString(c.(Centroid).String())
+		buffer.WriteString(",")
+	}
+	return fmt.Sprintf("TDigest<K=%d, weight=%.1f, centroids=[%s]>", t.compression, t.count, buffer.String())
+}
+
+func (t *TDigest) updateCentroid(c Centroid, mean float64, weight float64) {
+	if t.summary.Find(c) == nil {
+		panic(fmt.Sprintf("Trying to update a centroid that doesn't exist: %s. %s", c, t))
+	}
+
+	t.summary.Remove(c)
+	c.Update(mean, weight)
+	t.addCentroid(c)
+}
+
+func (t *TDigest) threshold(q float64) float64 {
+	return (4 * t.count * q * (1 - q)) / float64(t.compression)
+}
+
+func (t *TDigest) computeCentroidQuantile(c Centroid) float64 {
+	var cumSum float64 = 0
+	channel := t.exclusiveSliceUntilMean(c)
+	for item := range channel {
+		cumSum += item.count
+	}
+
+	return (c.count/2.0 + cumSum) / t.count
+}
+
+func (t *TDigest) addCentroid(c Centroid) {
+	current := t.summary.Find(c)
+
+	if current != nil {
+		t.summary.Remove(current)
+		c.Update(current.(Centroid).mean, current.(Centroid).count)
+	}
+
+	t.summary.Add(c)
+}
+
+func (t *TDigest) findNearestCentroids(c Centroid) []Centroid {
+	ceil, floor := t.ceilingAndFloorItems(c)
+
+	if ceil == InvalidCentroid && floor == InvalidCentroid {
+		panic("findNearestCentroids called on an empty tree")
+	}
+
+	if ceil == InvalidCentroid {
+		return []Centroid{floor}
+	}
+
+	if floor == InvalidCentroid {
+		return []Centroid{ceil}
+	}
+
+	if math.Abs(floor.mean-c.mean) < math.Abs(ceil.mean-c.mean) {
+		return []Centroid{floor}
+	} else if math.Abs(floor.mean-c.mean) == math.Abs(ceil.mean-c.mean) && !floor.Equals(ceil) {
+		return []Centroid{floor, ceil}
+	} else {
+		return []Centroid{ceil}
+	}
+}
+
+func (t *TDigest) getSurroundingWith(c Centroid, cmp func(a, b interface{}) bool) (Centroid, Centroid) {
+	ceiling, floor := InvalidCentroid, InvalidCentroid
+	for item := range t.summary.Iter() {
+		if ceiling == InvalidCentroid && cmp(c, item) {
+			ceiling = item.(Centroid)
+		}
+		if cmp(item, c) {
+			floor = item.(Centroid)
+		}
+	}
+	return ceiling, floor
+}
+
+func (t *TDigest) ceilingAndFloorItems(c Centroid) (Centroid, Centroid) {
+	// ceiling => smallest key greater than or equals to key
+	// floor   => greatest key less than or equals to key
+	return t.getSurroundingWith(c, centroidLessOrEquals)
+}
+
+func (t *TDigest) successorAndPredecessorItems(c Centroid) (Centroid, Centroid) {
+	// FIXME This can be way cheaper if done directly on the tree nodes
+	return t.getSurroundingWith(c, centroidLess)
+}
+
+func (t *TDigest) exclusiveSliceUntilMean(c Centroid) <-chan Centroid {
+	channel := make(chan Centroid)
+	go func() {
+		for item := range t.summary.Iter() {
+			if centroidLess(item, c) {
+				channel <- item.(Centroid)
+			} else {
+				break
+			}
+		}
+		close(channel)
+	}()
+	return channel
+}
---
+++ tdigest_test.go
@@ -0,0 +1,183 @@
+package tdigest
+
+import (
+	"math"
+	"math/rand"
+	"testing"
+)
+
+func TestCentroid(t *testing.T) {
+
+	c1 := Centroid{mean: 0.4, count: 1}
+	c2 := Centroid{mean: 0.4, count: 1}
+	c3 := Centroid{mean: 0.4, count: 2}
+
+	if c1.Equals(c2) != c2.Equals(c1) {
+		t.Errorf("Equality is not commutative: c1=%s c2=%s", c1, c2)
+	}
+
+	if !c1.Equals(c2) {
+		t.Errorf("C1 (%s) should be equals to C2 (%s)", c1, c2)
+	}
+
+	if c1.Equals(c3) != false {
+		t.Errorf("C1 (%s) should NOT be equals to C2 (%s)", c1, c3)
+	}
+
+	countBefore := c1.count
+	c1.Update(1, 1)
+
+	if c1.count <= countBefore || c1.count != countBefore+1 {
+		t.Errorf("Update didn't do what was expected to C1 (%s)", c1)
+	}
+}
+
+func TestCeilingAndFloor(t *testing.T) {
+	tdigest := New(100)
+
+	ceil, floor := tdigest.ceilingAndFloorItems(Centroid{1, 1})
+
+	if ceil != InvalidCentroid || floor != InvalidCentroid {
+		t.Errorf("Empty centroids must return invalid ceiling and floor items")
+	}
+
+	c1 := Centroid{mean: 0.4, count: 1}
+	tdigest.addCentroid(c1)
+
+	ceil, floor = tdigest.ceilingAndFloorItems(Centroid{0.3, 1})
+
+	if floor != InvalidCentroid || !c1.Equals(ceil) {
+		t.Errorf("Expected to find a floor and NOT find a ceiling. ceil=%s, floor=%s", ceil, floor)
+	}
+
+	ceil, floor = tdigest.ceilingAndFloorItems(Centroid{0.5, 1})
+
+	if ceil != InvalidCentroid || !c1.Equals(floor) {
+		t.Errorf("Expected to find a ceiling and NOT find a floor. ceil=%s, floor=%s", ceil, floor)
+	}
+
+	c2 := Centroid{mean: 0.1, count: 2}
+	tdigest.addCentroid(c2)
+
+	ceil, floor = tdigest.ceilingAndFloorItems(Centroid{0.2, 1})
+
+	if !c1.Equals(ceil) || !c2.Equals(floor) {
+		t.Errorf("Expected to find a ceiling and a floor. ceil=%s, floor=%s", ceil, floor)
+	}
+
+	c3 := Centroid{mean: 0.21, count: 3}
+	tdigest.addCentroid(c3)
+
+	ceil, floor = tdigest.ceilingAndFloorItems(Centroid{0.2, 1})
+
+	if !c3.Equals(ceil) || !c2.Equals(floor) {
+		t.Errorf("Ceil should've shrunk. ceil=%s, floor=%s", ceil, floor)
+	}
+
+	c4 := Centroid{mean: 0.1999, count: 1}
+	tdigest.addCentroid(c4)
+
+	ceil, floor = tdigest.ceilingAndFloorItems(Centroid{0.2, 1})
+
+	if !c3.Equals(ceil) || !c4.Equals(floor) {
+		t.Errorf("Floor should've shrunk. ceil=%s, floor=%s", ceil, floor)
+	}
+
+	ceil, floor = tdigest.ceilingAndFloorItems(Centroid{10, 1})
+
+	if ceil != InvalidCentroid {
+		t.Errorf("Expected an invalid ceil. Got %s", ceil)
+	}
+
+	ceil, floor = tdigest.ceilingAndFloorItems(Centroid{0.0001, 12})
+
+	if floor != InvalidCentroid {
+		t.Errorf("Expected an invalid floor. Got %s", floor)
+	}
+
+	ceil, floor = tdigest.ceilingAndFloorItems(c4)
+
+	if !floor.Equals(ceil) || floor == InvalidCentroid {
+		t.Errorf("ceiling and floor of an existing item should be the item itself")
+	}
+}
+
+func TestTInternals(t *testing.T) {
+
+	tdigest := New(100)
+
+	tdigest.addCentroid(Centroid{mean: 0.4, count: 1})
+	tdigest.addCentroid(Centroid{mean: 0.5, count: 1})
+
+	if tdigest.summary.Len() != 2 {
+		t.Errorf("Expected size 2, got %d", tdigest.summary.Len())
+	}
+
+	tdigest.addCentroid(Centroid{mean: 0.4, count: 2})
+	tdigest.addCentroid(Centroid{mean: 0.4, count: 3})
+
+	if tdigest.summary.Len() != 2 {
+		t.Errorf("Adding centroids of same mean shouldn't change size")
+	}
+
+	y := tdigest.summary.Find(Centroid{mean: 0.4})
+
+	if y.(Centroid).count != 6 || y.(Centroid).mean != 0.4 {
+		t.Errorf("Adding centroids with same mean should increment the count only. Got %s", y.(Centroid))
+	}
+
+}
+
+func assertDifferenceSmallerThan(tdigest *TDigest, p float64, m float64, t *testing.T) {
+	tp := tdigest.Percentile(p)
+	if math.Abs(tp-p) >= m {
+		t.Errorf("T-Digest.Percentile(%.4f) = %.4f. Diff (%.4f) >= %.4f", p, tp, math.Abs(tp-p), m)
+	}
+}
+
+func TestUniformDistribution(t *testing.T) {
+	tdigest := New(10)
+
+	for i := 0; i < 10000; i++ {
+		tdigest.Update(rand.Float64(), 1)
+	}
+
+	assertDifferenceSmallerThan(tdigest, 0.5, 0.02, t)
+	assertDifferenceSmallerThan(tdigest, 0.1, 0.01, t)
+	assertDifferenceSmallerThan(tdigest, 0.9, 0.01, t)
+	assertDifferenceSmallerThan(tdigest, 0.01, 0.005, t)
+	assertDifferenceSmallerThan(tdigest, 0.99, 0.005, t)
+	assertDifferenceSmallerThan(tdigest, 0.001, 0.001, t)
+	assertDifferenceSmallerThan(tdigest, 0.999, 0.001, t)
+}
+
+func TestIntegers(t *testing.T) {
+	tdigest := New(100)
+
+	tdigest.Update(1, 1)
+	tdigest.Update(2, 1)
+	tdigest.Update(3, 1)
+
+	if tdigest.Percentile(0.5) != 2 {
+		t.Errorf("Expected p(0.5) = 2, Got %.2f instead", tdigest.Percentile(0.5))
+	}
+
+	tdigest = New(100)
+
+	for _, i := range []float64{1, 2, 2, 2, 2, 2, 2, 2, 3} {
+		tdigest.Update(i, 1)
+	}
+
+	if tdigest.Percentile(0.5) != 2 {
+		t.Errorf("Expected p(0.5) = 2, Got %.2f instead", tdigest.Percentile(0.5))
+	}
+
+	var tot float64 = 0
+	for i := range tdigest.summary.Iter() {
+		tot += i.(Centroid).count
+	}
+
+	if tot != 9 {
+		t.Errorf("Expected the centroid count to be 9, Got %.2f instead", tot)
+	}
+}
